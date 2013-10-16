ce07ac3e18803798cea82f81406badf44b26d76c
Carlos Garcia de marina <garciademarina@gmail.com>
2013-10-16 11:15:40 +0200

    stats improvements

--- contact_counter/ModelContactCounter.php
+++ contact_counter/ModelContactCounter.php
@@ -136,6 +136,19 @@
             }
         }
 
+        public function getTotalContactsByItemId($item_id)
+        {
+            $sql = 'SELECT  i_contacts as total FROM '.$this->getTable(). ' WHERE fk_i_item_id = ' . $item_id ;
+            $result = $this->dao->query($sql) ;
+
+            $items = $result->result() ;
+            if( is_array($items) && count($items)>0) {
+                return $items[0]['total'];
+            } else {
+                return 0;
+            }
+        }
+
         /**
          * Delete house attributes given a item id
          * @param type $item_id
--- contact_counter/index.php
+++ contact_counter/index.php
@@ -11,6 +11,7 @@
  */
 
 require_once 'ModelContactCounter.php';
+require_once 'StatsContactCounter.php';
 
 //Delete item
 osc_add_hook('delete_item', 'contact_counter_delete');
@@ -43,4 +44,58 @@ function contact_counter_uninstall() {
     // remove table structure
     ModelContactCounter::newInstance()->uninstall();
 }
+
+// add link to item contact stats page / manage listing, under more actions link
+function contact_counter_more_actions_link( $options_more, $aRow) {
+    // get number of contact by listing
+    $num_contacts = ModelContactCounter::newInstance()->getTotalContactsByItemId($aRow['pk_i_id']);
+    $aux = $options_more;
+    $aux[] = '<a href="' . osc_route_admin_url('stats-contact-counter', array('id' => $aRow['pk_i_id'])) . '">' . sprintf(__('<b>%s</b> contacts +'),$num_contacts) . '</a>';
+    return $aux;
+}
+osc_add_hook('actions_manage_items', 'contact_counter_more_actions_link');
+
+// admin menu
+function contact_counter_admin_menu() {
+    if (osc_version() < 320) {
+        echo '<h3><a href="#">'.__('Contact counter', 'contact_counter').'</a></h3>
+            <ul>
+                <li><a href="' . osc_admin_configure_plugin_url("contact_counter/admin/stats.php") . '">&raquo; ' . __('Contact stats', 'contact_counter') . '</a></li>
+            </ul>';
+    } else {
+        osc_add_admin_submenu_page('stats', __('View contact stats', 'contact_counter'), osc_route_admin_url('stats-contact-counter', array('id' => '')), '', 'administrator');
+    }
+}
+
+if(osc_version()<320) {
+    osc_add_hook('admin_menu', 'contact_counter_admin_menu');
+} else {
+    osc_add_hook('admin_menu_init', 'contact_counter_admin_menu');
+}
+
+//  routes
+osc_add_route('stats-contact-counter', 'stats-contact-counter/(.+)/([0-9]+)', 'stats-contact-counter/{type_stat}/{id}', osc_plugin_folder(__FILE__).'admin/stats.php');
+
+// custom title/header stats page
+if(Params::getParam('page') == 'plugins' && Params::getParam('file') == 'contact_counter/admin/stats.php' || Params::getParam('route') == 'stats-contact-counter') {
+    osc_add_hook('admin_header',        'contact_counter_remove_title_header');
+    osc_add_hook('admin_page_header',   'contact_counter_PageHeader_stats');
+    osc_add_filter('admin_title',       'contact_counter_customPageTitle');
+}
+
+function contact_counter_remove_title_header() {
+    osc_remove_hook('admin_page_header','customPageHeader');
+}
+
+function contact_counter_PageHeader_stats() { ?>
+    <h1><?php _e('Contact stats', 'contact_counter'); ?>
+        <a href="#" class="btn ico ico-32 ico-help float-right"></a>
+    </h1>
+<?php
+}
+function contact_counter_customPageTitle($string) {
+    return sprintf(__('Contact Statistics &raquo; %s'), $string);
+}
+
+
 ?>
